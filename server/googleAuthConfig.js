189aa6562018cb57f5062c3f6e730a9d38191362
tarzak <jobtarzak@gmail.com>
2016-03-01 23:12:56 +0200

    add google authentication

---
+++ server/googleAuthConfig.js
@@ -0,0 +1,10 @@
+ServiceConfiguration.configurations.upsert(
+    { service: "google" },
+    {
+        $set: {
+            clientId: "276759588095-a5hp23sjldmr5vtolgv5el65v57456pg.apps.googleusercontent.com",
+            loginStyle: "popup",
+            secret: "Jl81j0L9pUQih8cRg_VRjQWg"
+        }
+    }
+);
